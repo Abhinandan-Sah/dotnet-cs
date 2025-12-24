b9271250978ca55fd8dd6833dc3f8c4a59952605
Abhinandan Sah <abhinandansah222@gmail.com>
2025-12-24 12:56:41 +0530

    Add initial project file for day6_Interface_others with .NET SDK configuration

---
+++ day6_Interface_others/Program.cs
@@ -0,0 +1,13 @@
+﻿using day6.InterfaceExample;
+
+namespace day6_Interface_others
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            IPrint iprint = new Document("Maze Runner");
+            iprint.print();
+        }
+    }
+}
